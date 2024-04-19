6131a8da2c21ae0acf267e175e6fd9c1f971ce4e
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-04-19 10:02:00 +0300

    Create firstTask.c

---
+++ firstTask.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void sortDescending(int *a, int *b, int *c);
+
+int main() {
+    int num1, num2, num3;
+
+    printf("Въведете първото число: ");
+    scanf("%d", &num1);
+    printf("Въведете второто число: ");
+    scanf("%d", &num2);
+    printf("Въведете третото число: ");
+    scanf("%d", &num3);
+
+ 
+    sortDescending(&num1, &num2, &num3);
+
+    printf("Сортирани в низходящ ред: %d %d %d\n", num1, num2, num3);
+
+    return 0;
+}
+
+void sortDescending(int *a, int *b, int *c) {
+    int temp;
+
+    if (*a < *b) {
+        temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+    if (*b < *c) {
+        temp = *b;
+        *b = *c;
+        *c = temp;
+    }
+    if (*a < *b) {
+        temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+}
